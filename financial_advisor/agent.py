84bcd446fb7a0636f72dbf2c4b4f22b12d3437ac
dlwls5201 <dlwls5201@naver.com>
2025-11-15 17:15:03 +0900

    fix import issue

--- financial_advisor/agent.py
+++ financial_advisor/agent.py
@@ -3,7 +3,9 @@ from google.adk.tools import ToolContext
 from google.adk.agents import Agent
 from google.adk.tools.agent_tool import AgentTool
 from google.adk.models.lite_llm import LiteLlm
-from .sub_agents import news_analyst, data_analyst, financial_analyst
+from .sub_agents.data_analyst import data_analyst
+from .sub_agents.financial_analyst import financial_analyst
+from .sub_agents.news_analyst import news_analyst
 from .prompt import PROMPT
 
 MODEL = LiteLlm("openai/gpt-4o")
